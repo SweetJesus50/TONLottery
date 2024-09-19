cdc1e83408b6c95245c56696c72198c580fb0b8f
SweetJesus <methoxyn50@gmail.com>
2024-09-19 16:24:22 +0500

    fixed bank_total_cash

--- scripts/deployLottery.ts
+++ scripts/deployLottery.ts
@@ -1,13 +1,44 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { Lottery } from '../wrappers/Lottery';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const lottery = provider.open(Lottery.createFromConfig({}, await compile('Lottery')));
+    const lottery = provider.open(Lottery.createFromConfig({
+        adminAddress: provider.sender().address as Address,         // адрес админа (в данном случае будет админ тот, кто отправляет транзакцию на деплой)
+        bankWalletAddress: Address.parse(""),                       // адрес банка, куда придет процент (просто скопировать и вставить адрес в кавычки)
+        cycleLength: 5,                                             // количество циклов (транзакций)
+        betAmount: toNano("0.1")                                    // сумма ставки (1,2,3...n)
+    }, await compile('Lottery')));
 
-    await lottery.sendDeploy(provider.sender(), toNano('0.05'));
+    await lottery.sendDeploy(provider.sender(), toNano('1'));       // деплой
 
-    await provider.waitForDeploy(lottery.address);
+    await provider.waitForDeploy(lottery.address, 25);
 
-    // run methods on `lottery`
+    // @ ДОП МЕТОДЫ @ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // await lottery.sendChangeBet(provider.sender(), toNano("0.01"), toNano("20"))                    // смена ставки (валидна только после конца цикла, до начала нового)
+
+    // await lottery.sendChangeCycleLength(provider.sender(), toNano("0.01"), 10)                      // смена циклов (транзакций) (так же валидна только после конца цикла, до начала нового)
+
+    // await lottery.sendChangeBankWalletAddress(provider.sender(), toNano("0.01"), Address.parse("")) // смена адреса банка (просто скопировать и вставить адрес в кавычки)
+
+    // await lottery.sendWithdrawJettons(provider.sender(), toNano("0.055"), {                         // отправить "случайно" попавшие жетоны на контракт (вариант без toAddress отправит токены на zero-address.ton)
+    //     jettonWalletAddress: Address.parse("Адрес жетон кошелька контракта"),
+    //     jettonAmount: toNano("10")
+    // })
+
+    // await lottery.sendWithdrawJettons(provider.sender(), toNano("0.055"), {                         // отправить "случайно" попавшие жетоны на контракт (вариант с toAddress отправит токены нужному адресу)
+    //     jettonWalletAddress: Address.parse("Адрес жетон кошелька контракта"),
+    //     jettonAmount: toNano("10"),
+    //     toAddress: Address.parse("Адрес обычного кошелька юзера (или контракта), кому нено отправить")
+    // })
+
+    // await lottery.sendWithdrawNft(provider.sender(), toNano("0.055"), {                             // отправить "случайно" попавшую NFT на контракт (вариант без toAddress отправит NFT на zero-address.ton)
+    //     nftAddress: Address.parse("Адрес нфт, которую надо отправить")
+    // })
+
+    // await lottery.sendWithdrawNft(provider.sender(), toNano("0.055"), {                             // отправить "случайно" попавшую NFT на контракт (вариант с toAddress отправит NFT нужному адресу)
+    //     nftAddress: Address.parse("Адрес NFT, которую надо отправить"),
+    //     toAddress: Address.parse("Адрес кому нужно отправить NFT (кошелек или контракт)")
+    // })
 }
--- wrappers/Lottery.ts
+++ wrappers/Lottery.ts
@@ -1,9 +1,23 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Builder, Cell, Contract, contractAddress, ContractProvider, Dictionary, DictionaryValue, Sender, SendMode, toNano } from '@ton/core';
 
-export type LotteryConfig = {};
+export type LotteryConfig = {
+    adminAddress: Address,
+    bankWalletAddress: Address,
+    cycleLength: number,
+    betAmount: bigint
+};
 
 export function lotteryConfigToCell(config: LotteryConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+                .storeBit(0)
+                .storeAddress(config.adminAddress)
+                .storeAddress(config.bankWalletAddress)
+                .storeDict(Dictionary.empty())
+                .storeUint(config.cycleLength, 32)
+                .storeCoins(config.betAmount)
+                .storeUint(0, 32)
+                .storeCoins(0)
+            .endCell();
 }
 
 export class Lottery implements Contract {
@@ -23,7 +37,93 @@ export class Lottery implements Contract {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: beginCell().storeUint(0xfe0f6f57, 32).storeUint(Math.floor(Date.now() / 1000), 64).endCell(),
         });
+    }
+
+    async sendChangeBet(provider: ContractProvider, via: Sender, value: bigint, newBet: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0x367482d2, 32).storeUint(Math.floor(Date.now() / 1000), 64).storeCoins(newBet).endCell(),
+        });
+    }
+
+    async sendChangeCycleLength(provider: ContractProvider, via: Sender, value: bigint, newCycleLength: number) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0xd5eab682, 32).storeUint(Math.floor(Date.now() / 1000), 64).storeUint(newCycleLength, 32).endCell(),
+        });
+    }
+
+    async sendChangeBankWalletAddress(provider: ContractProvider, via: Sender, value: bigint, newBankAddress: Address) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0xf06c0892, 32).storeUint(Math.floor(Date.now() / 1000), 64).storeAddress(newBankAddress).endCell(),
+        });
+    }
+
+    async sendWithdrawJettons(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        jettonWalletAddress: Address,
+        jettonAmount: bigint,
+        toAddress?: Address
+    }) {
+        let msgBody = beginCell()
+                        .storeUint(0xba2c493a, 32)
+                        .storeUint(Math.floor(Date.now() / 1000), 64)
+                        .storeAddress(opts.jettonWalletAddress)
+                        .storeCoins(opts.jettonAmount)
+
+        if(opts.toAddress != undefined) {
+            msgBody.storeAddress(opts.toAddress);
+        }                
+
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msgBody.endCell()
+        });
+    }
+
+    async sendWithdrawNft(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        nftAddress: Address,
+        toAddress?: Address
+    }) {
+        let msgBody = beginCell()
+                        .storeUint(0x683da743, 32)
+                        .storeUint(Math.floor(Date.now() / 1000), 64)
+                        .storeAddress(opts.nftAddress)
+                        
+        if(opts.toAddress != undefined) {
+            msgBody.storeAddress(opts.toAddress);
+        }                
+
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: msgBody.endCell()
+        });
+    }
+
+    // GET METHODS
+
+    async getLotteryData(provider: ContractProvider) {
+        let { stack } = await provider.get('get_lottery_data', [])
+
+        return {
+            adminAddress: stack.readAddress(),
+            bankWalletAddress: stack.readAddress(),
+            addrList: stack.readCell(),
+            cycleLength: stack.readNumber(),
+            betAmount: stack.readBigNumber(),
+            txCount: stack.readNumber(),
+            bankTotalCash: stack.readBigNumber()
+        }
+    }
+
+    async getLotteryStatus(provider: ContractProvider) {
+        return (await provider.get('get_lottery_status', [])).stack.readBoolean()
     }
 }
